e54e62b8b65c0f0c693e5a30242d921a81463a96
tia-tai <taiyulin14@gmail.com>
2025-10-20 13:28:44 -0400

    created static animals list in animal class

--- src/Animals.java
+++ src/Animals.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
+
 public class Animals {
     private int rank;
     private String name;
     private float length;
     private int maxMass;
+    static ArrayList<Animals> animals = new ArrayList<>();
 
     public Animals(int rank, String name, float length, int maxMass) {
         this.rank = rank;
         this.name = name;
         this.length = length;
         this.maxMass = maxMass;
+        animals.add(this);
+    }
+
+    public static ArrayList<Animals> getAnimals() {
+        return animals;
+    }
+
+    public static void setAnimals(ArrayList<Animals> animals) {
+        Animals.animals = animals;
     }
 
     public int getRank() {
--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Animals> animals = new ArrayList<>();
 
-        Mammal mammal1 = new Mammal(1, "Blue Whale", 79f, 418878, 242508);
-        Fish fish1 = new Fish(1, "Whale Shark", 61.7f, 47000, "https://en.wikipedia.org/wiki/File:Similan_Dive_Center_-_great_whale_shark.jpg", "Rhincodon typus", "Chondrichthyes", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Whaleshark_scale.jpg/250px-Whaleshark_scale.jpg");
-        Birds bird1 = new Birds(1,"Snowy Albatross",53.1f,28,"Diomedea Exulans",3.7f,"Vulnerable");
+        new Mammal(1, "Blue Whale", 79f, 418878, 242508);
+        new Fish(1, "Whale Shark", 61.7f, 47000, "https://en.wikipedia.org/wiki/File:Similan_Dive_Center_-_great_whale_shark.jpg", "Rhincodon typus", "Chondrichthyes", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Whaleshark_scale.jpg/250px-Whaleshark_scale.jpg");
+        new Birds(1,"Snowy Albatross",53.1f,28,"Diomedea Exulans",3.7f,"Vulnerable");
 
-        animals.add(mammal1);
-        animals.add(fish1);
-        animals.add(bird1);
-
-        System.out.println(mammal1);
-        System.out.println(fish1);
-        System.out.println(bird1);
-
-        for (Animals animal: animals) {
+        for (Animals animal: Animals.getAnimals()) {
             System.out.println(animal);
         }
     }
