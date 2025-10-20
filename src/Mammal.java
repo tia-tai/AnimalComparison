971c74f97b4b2e96ebec4770f6c5d278746c3410
tia-tai <taiyulin14@gmail.com>
2025-10-20 13:30:33 -0400

    created static animals list in mammal class

--- src/Mammal.java
+++ src/Mammal.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
+
 public class Mammal extends Animals{
     private int avgMass;
+    static ArrayList<Mammal> mammals = new ArrayList<Mammal>();
 
     public Mammal(int rank, String name, float length, int maxMass, int avgMass) {
         super(rank, name, length, maxMass);
         this.avgMass = avgMass;
+        mammals.add(this);
+    }
+
+    public static ArrayList<Mammal> getMammals() {
+        return mammals;
+    }
+
+    public static void setMammals(ArrayList<Mammal> mammals) {
+        Mammal.mammals = mammals;
     }
 
     public long getAvgMass() {
